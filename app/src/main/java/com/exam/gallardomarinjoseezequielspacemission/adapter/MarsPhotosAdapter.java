39a103973cbeccd42aa89c0e232463d8b7c1ca81
zeku87 <nocomment182@gmail.com>
2018-07-04 13:40:49 +0200

    Odd index uses #FFEDD3 background color

--- app/src/main/java/com/exam/gallardomarinjoseezequielspacemission/adapter/MarsPhotosAdapter.java
+++ app/src/main/java/com/exam/gallardomarinjoseezequielspacemission/adapter/MarsPhotosAdapter.java
@@ -1,5 +1,8 @@
 package com.exam.gallardomarinjoseezequielspacemission.adapter;
 
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,6 +40,15 @@ public class MarsPhotosAdapter extends RecyclerView.Adapter<MarsPhotosAdapter.Ma
 
     @Override
     public void onBindViewHolder(final MarsPhotosAdapter.MarsPhotosViewHolder holder, int position) {
+
+        if(position % 2 == 1){
+            holder.itemView.setBackgroundColor(Color.parseColor("#FFEDD3"));
+        }
+        else
+        {
+            holder.itemView.setBackgroundColor(Color.parseColor("#FFF"));
+        }
+
         holder.textViewEarthDate.setText(marsPhotosArrayList.get(position).getEarthDate());
         holder.textViewCameraFullName.setText(marsPhotosArrayList.get(position).getCamera());
         final String marsPhotoURL = marsPhotosArrayList.get(position).getImgSrc();
@@ -61,11 +73,13 @@ public class MarsPhotosAdapter extends RecyclerView.Adapter<MarsPhotosAdapter.Ma
     }
 
     class MarsPhotosViewHolder extends RecyclerView.ViewHolder{
+        CardView cardView;
         TextView textViewEarthDate, textViewCameraFullName;
         ImageView imageViewMarsPhoto;
 
         MarsPhotosViewHolder(View view){
             super(view);
+            cardView = view.findViewById(R.id.cardView);
             textViewEarthDate = view.findViewById(R.id.textViewEarthDate);
             textViewCameraFullName = view.findViewById(R.id.textViewCameraFullName);
             imageViewMarsPhoto = view.findViewById(R.id.imageViewMarsPhoto);
