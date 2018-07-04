55f88f7d7091673ec9b68511750efc3d77820bde
zeku87 <nocomment182@gmail.com>
2018-07-04 13:43:41 +0200

    Clean

--- app/src/main/java/com/exam/gallardomarinjoseezequielspacemission/main/MainContract.java
+++ app/src/main/java/com/exam/gallardomarinjoseezequielspacemission/main/MainContract.java
@@ -10,23 +10,14 @@ import java.util.ArrayList;
 
 public interface MainContract {
 
-    /**
-     * Call when user interacts with the view and other when view OnDestroy()
-     * */
     interface Presenter{
 
         void onDestroy();
-
-        void onRefreshButtonClick();
 
         void requestDataFromServer();
 
     }
 
-    /**
-     * showProgress() and hideProgress() would be used for displaying and hiding the progressBar
-     * while the setDataToRecyclerView and onResponseFailure is fetched from the GetNoticeInteractorImpl class
-     **/
     interface MainView {
 
         void showProgress();
